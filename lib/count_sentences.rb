25b6d81fd5f5d30140fe63cffc60383059bfdfee
Luis Martinez <luis_mmartinez@live.com>
2020-12-31 04:57:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    raw_array=self.split(/[.?!]/)
+    final_array=raw_array.delete_if {|i| i ==""}
+    final_count=final_array.count
+    final_count
   end
 end
